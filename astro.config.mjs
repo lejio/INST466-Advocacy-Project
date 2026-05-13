160ee4bc4a5db35ef35d101d2438151d1b9c1e17
Gene Ni <geneisjuan@gmail.com>
2026-05-13 15:32:49 -0400

    setup for github actions and pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://INST466-Advocacy-Project.github.io",
   fonts: [{
       provider: fontProviders.google(),
       name: "Roboto Mono",
