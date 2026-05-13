1478f1aaf8b543c1079758f943011f818a39f2c0
Gene Ni <geneisjuan@gmail.com>
2026-05-13 13:47:08 -0400

    work in progress checkpoint
    added tailwind, mono font, navbar, footer, the various tabs as well as completed home and purpose pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fontProviders } from 'astro/config';
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [{
+      provider: fontProviders.google(),
+      name: "Roboto Mono",
+      cssVariable: "--font-roboto-mono",
+  }],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
